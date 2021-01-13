52f14579ecf42e3e6b3b7a256e6e21ba4a378822
zhangsiyuan <1187178055@qq.com>
2021-01-13 09:08:17 +0800

    2021.01.13 链表相交问题 三种解法

--- src/test20210112/Test06.java
+++ src/test20210112/Test06.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  * 给你一个单链表的引用结点head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
  * 请你返回该链表所表示数字的 十进制值 。
  */
-public class Test06NO {
+public class Test06 {
     public int getDecimalValue(ListNode head) {
         ListNode cur = head;
         StringBuilder sb = new StringBuilder();
